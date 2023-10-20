75f1ea934ca65e4204ff80106d1dd659a6c709bd
kain-1910 <pvcong1910@gmail.com>
2023-10-20 09:48:41 +0700

    upload layout

---
+++ Project_BE_Web/wwwroot/js/layout.js
@@ -0,0 +1,10 @@
+﻿const sidebarBtn = document.querySelector('.sidebar-btn');
+const layout = document.querySelector('.layout');
+const closeLayoutBtn = document.querySelector('.close-sidebar-btn');
+sidebarBtn.addEventListener('click', () => {
+    layout.classList.add('active');
+})
+closeLayoutBtn.addEventListener('click', () => {
+    layout.classList.remove('active');
+})
+
